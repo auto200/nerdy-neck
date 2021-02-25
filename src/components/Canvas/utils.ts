932ef44f1e8d90aa21f560e7b0f7cdb6a6b0a333
auto200 <michal.warac@gmail.com>
2021-02-25 19:41:30 +0100

    Organize Canvas component and its functions

---
+++ src/components/Canvas/utils.ts
@@ -0,0 +1,45 @@
+import { Vector2D } from "@tensorflow-models/posenet/dist/types";
+
+export const degreeBetweenPoints = (
+  a: Vector2D,
+  b: Vector2D,
+  round: boolean = true
+) => {
+  const dy = Math.abs(a.y - b.y);
+  const dx = Math.abs(a.x - b.x);
+  const deg = Math.abs(90 - Math.atan2(dy, dx) * (180 / Math.PI));
+  if (round) {
+    return Math.round(deg);
+  }
+  return deg;
+};
+
+export const drawPoint = (
+  ctx: CanvasRenderingContext2D,
+  point: Vector2D,
+  color: string,
+  roundPoints: boolean = true
+) => {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  if (roundPoints) {
+    ctx.arc(Math.round(point.x), Math.round(point.y), 5, 0, 2 * Math.PI);
+  } else {
+    ctx.arc(point.x, point.y, 5, 0, 2 * Math.PI);
+  }
+  ctx.fill();
+};
+
+export const drawLine = (
+  ctx: CanvasRenderingContext2D,
+  start: Vector2D,
+  end: Vector2D,
+  color: string
+) => {
+  ctx.beginPath();
+  ctx.moveTo(start.x, start.y);
+  ctx.lineTo(end.x, end.y);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = 2;
+  ctx.stroke();
+};
