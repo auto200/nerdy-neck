ab5ea78684525699386fffc9ab96fabf215f0f58
auto200 <michal.warac@gmail.com>
2021-02-28 21:05:59 +0100

    Move config ui to new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,26 @@
 import { useEffect, useRef, useState } from "react";
 import "@tensorflow/tfjs";
-import * as posenet from "@tensorflow-models/posenet";
+import { load as loadPosenet, PoseNet, Pose } from "@tensorflow-models/posenet";
 import Canvas from "./components/Canvas";
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Heading,
-  NumberInput,
-  NumberInputField,
-  Select,
-  Switch,
-  VStack,
-} from "@chakra-ui/react";
+import { Box, Button, Flex, Select } from "@chakra-ui/react";
 import { useConfig } from "./contexts/Config";
+import Config from "./components/Config";
 
 const WIDTH = 600;
 const HEIGHT = 500;
 
 function App() {
   const [cams, setCams] = useState<MediaDeviceInfo[]>();
-  const [cam, setCam] = useState<MediaDeviceInfo>();
-  const [net, setNet] = useState<posenet.PoseNet>();
+  const [currentCamIndex, setCurrentCamIndex] = useState<number>(0);
+  const [net, setNet] = useState<PoseNet>();
   const [mediaLoaded, setMediaLoaded] = useState(false);
-  const [pose, setPose] = useState<posenet.Pose>();
+  const [pose, setPose] = useState<Pose>();
   const [running, setRunning] = useState<boolean>(false);
+  const { config } = useConfig();
+
   const mediaRef = useRef<HTMLVideoElement>(null);
   const runningRef = useRef(running);
   const getPoseIntervalRef = useRef<number>();
-  const { config, dispatch: dispatchConfig } = useConfig();
 
   useEffect(() => {
     const init = async () => {
@@ -38,9 +28,8 @@ function App() {
         const devices = await navigator.mediaDevices.enumerateDevices();
         const cams = devices.filter(({ kind }) => kind === "videoinput");
         setCams(cams);
-        setCam(cams[0]);
 
-        setNet(await posenet.load());
+        setNet(await loadPosenet());
       } catch (err) {
         console.log(err);
       }
@@ -49,16 +38,16 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if (!cam) return;
-
     const getStream = async () => {
+      if (!cams) return;
+
       try {
         const stream = await navigator.mediaDevices.getUserMedia({
           video: {
             facingMode: "user",
             width: WIDTH,
             height: HEIGHT,
-            deviceId: cam.deviceId,
+            deviceId: cams[currentCamIndex]?.deviceId,
           },
         });
         if (mediaRef.current) {
@@ -69,7 +58,7 @@ function App() {
       }
     };
     getStream();
-  }, [cam]);
+  }, [cams, currentCamIndex]);
 
   useEffect(() => {
     runningRef.current = running;
@@ -106,10 +95,12 @@ function App() {
           {cams?.length && (
             <Select
               w="60"
-              value={cam?.label}
+              value={cams[currentCamIndex]?.label}
               onChange={(e) => {
-                const cam = cams.find(({ label }) => label === e.target.value);
-                cam && setCam(cam);
+                const cam = cams.findIndex(
+                  ({ label }) => label === e.target.value
+                );
+                cam && setCurrentCamIndex(cam);
               }}
             >
               {cams.map(({ label }) => (
@@ -127,184 +118,9 @@ function App() {
           </Button>
         </Flex>
       </Box>
-
-      <VStack p="5" pt="0">
-        <Heading as="h1">config:</Heading>
-        <Box>
-          <FormControl mt="6" mb="6">
-            <FormLabel htmlFor="body-side-switch" m="0">
-              Body side
-            </FormLabel>
-            <Box>
-              Left
-              <Switch
-                id="body-side-switch"
-                ml="2"
-                mr="2"
-                isChecked={config.bodySide === "right"}
-                onChange={() => dispatchConfig({ type: "TOGGLE_BODY_SIDE" })}
-                sx={{
-                  "& span[data-checked]": {
-                    background: "rgba(255, 255, 255, 0.24)",
-                  },
-                  "& span span": {
-                    background: "blue.200",
-                  },
-                  "& span[data-checked] span": {
-                    background: "blue.200",
-                  },
-                }}
-              />
-              Right
-            </Box>
-          </FormControl>
-
-          <CustomInput
-            id="frequency"
-            label="Check pose interval"
-            value={config.getPoseFrequency}
-            addDegreeSign={false}
-            onChange={(val) =>
-              dispatchConfig({
-                type: "SET_GET_POSE_FREQUENCY",
-                payload: val,
-              })
-            }
-          />
-
-          <CustomSwitch
-            id="ear-shoulder-monitoring-switch"
-            label="Ear-shoulder angle monitoring"
-            isChecked={config.earShoulderMonitoring.enabled}
-            onChange={() =>
-              dispatchConfig({
-                type: "TOGGLE_EAR_SHOULDER_MONITORING",
-              })
-            }
-          />
-          <CustomInput
-            id="ear-shoulder-angle"
-            label="Desired angle"
-            value={config.earShoulderMonitoring.angle}
-            onChange={(val) =>
-              dispatchConfig({
-                type: "SET_EAR_SHOULDER_ANGLE",
-                payload: val,
-              })
-            }
-          />
-          <CustomInput
-            id="ear-shoulder-tolerance"
-            label="Tolerance"
-            value={config.earShoulderMonitoring.tolerance}
-            onChange={(val) =>
-              dispatchConfig({
-                type: "SET_EAR_SHOULDER_TOLERANCE",
-                payload: val,
-              })
-            }
-          />
-
-          <CustomSwitch
-            id="shoulder-wrist-switch"
-            label="Shoulder-wrist angle monitoring"
-            isChecked={config.shoulderWristMonitoring.enabled}
-            onChange={() =>
-              dispatchConfig({
-                type: "TOGGLE_SHOULDER_WRIST_MONITORING",
-              })
-            }
-          />
-          <CustomInput
-            id="shoulder-wrist-angle"
-            label="Desired angle"
-            value={config.shoulderWristMonitoring.angle}
-            onChange={(val) =>
-              dispatchConfig({
-                type: "SET_SHOULDER_WRIST_ANGLE",
-                payload: val,
-              })
-            }
-          />
-          <CustomInput
-            id="shoulder-wrist-tolerance"
-            label="Tolerance"
-            value={config.shoulderWristMonitoring.tolerance}
-            onChange={(val) =>
-              dispatchConfig({
-                type: "SET_SHOULDER_WRIST_TOLERANCE",
-                payload: val,
-              })
-            }
-          />
-
-          <CustomSwitch
-            id="ban-knee-and-ankle-switch"
-            label="Ban-knee-and-ankle"
-            isChecked={config.banKneeAndAnkle}
-            onChange={() =>
-              dispatchConfig({
-                type: "TOGGLE_BAN_KNEE_AND_ANKLE",
-              })
-            }
-          />
-        </Box>
-      </VStack>
+      <Config />
     </Flex>
   );
 }
 
 export default App;
-
-const CustomSwitch = ({
-  id,
-  label,
-  ...rest
-}: {
-  id: string;
-  label: string;
-  [rest: string]: any;
-}) => {
-  return (
-    <FormControl display="flex" alignItems="center" mt="6">
-      <Switch id={id} mr="1" {...rest} />
-      <FormLabel htmlFor={id} m="0">
-        {label}
-      </FormLabel>
-    </FormControl>
-  );
-};
-
-const CustomInput = ({
-  id,
-  label,
-  value,
-  onChange,
-  addDegreeSign = true,
-  ...rest
-}: {
-  id: string;
-  label: string;
-  value: string | number;
-  onChange: (valueAsString: string, valueAsNumber: number) => void;
-  addDegreeSign?: boolean;
-  [rest: string]: any;
-}) => {
-  return (
-    <FormControl>
-      <FormLabel htmlFor={id} m="0">
-        {label}
-      </FormLabel>
-      <NumberInput
-        id={id}
-        mr="1"
-        value={value + `${addDegreeSign ? "°" : ""}`}
-        onChange={onChange}
-        {...rest}
-        min={0}
-      >
-        <NumberInputField />
-      </NumberInput>
-    </FormControl>
-  );
-};
---
+++ src/components/Config.tsx
@@ -0,0 +1,201 @@
+import {
+  Box,
+  chakra,
+  FormControl,
+  FormLabel,
+  Heading,
+  NumberInput,
+  NumberInputField,
+  Switch,
+  VStack,
+} from "@chakra-ui/react";
+import React from "react";
+import { useConfig } from "../contexts/Config";
+
+const CustomSwitch = ({
+  id,
+  label,
+  ...rest
+}: {
+  id: string;
+  label: string;
+  [rest: string]: any;
+}) => {
+  return (
+    <FormControl display="flex" alignItems="center" mt="6">
+      <Switch id={id} mr="1" {...rest} />
+      <FormLabel htmlFor={id} m="0">
+        {label}
+      </FormLabel>
+    </FormControl>
+  );
+};
+
+const CustomInput = ({
+  id,
+  label,
+  value,
+  onChange,
+  addDegreeSign = true,
+  ...rest
+}: {
+  id: string;
+  label: string;
+  value: string | number;
+  onChange: (valueAsString: string, valueAsNumber: number) => void;
+  addDegreeSign?: boolean;
+  [rest: string]: any;
+}) => {
+  return (
+    <FormControl>
+      <FormLabel htmlFor={id} m="0">
+        {label}
+      </FormLabel>
+      <NumberInput
+        id={id}
+        mr="1"
+        value={value + `${addDegreeSign ? "°" : ""}`}
+        onChange={onChange}
+        {...rest}
+        min={0}
+      >
+        <NumberInputField />
+      </NumberInput>
+    </FormControl>
+  );
+};
+
+const Config = () => {
+  const { config, dispatch: dispatchConfig } = useConfig();
+
+  return (
+    <VStack p="5" pt="0">
+      <Heading as="h1">config:</Heading>
+      <Box>
+        <FormControl mt="6" mb="6">
+          <FormLabel htmlFor="body-side-switch" m="0">
+            Body side
+          </FormLabel>
+          <Box>
+            <chakra.span color={config.bodySide === "left" ? "blue.200" : ""}>
+              Left
+            </chakra.span>
+            <Switch
+              id="body-side-switch"
+              ml="2"
+              mr="2"
+              isChecked={config.bodySide === "right"}
+              onChange={() => dispatchConfig({ type: "TOGGLE_BODY_SIDE" })}
+              sx={{
+                "& span[data-checked]": {
+                  background: "rgba(255, 255, 255, 0.24)",
+                },
+                "& span span": {
+                  background: "blue.200",
+                },
+                "& span[data-checked] span": {
+                  background: "blue.200",
+                },
+              }}
+            />
+            <chakra.span color={config.bodySide === "right" ? "blue.200" : ""}>
+              Right
+            </chakra.span>
+          </Box>
+        </FormControl>
+
+        <CustomInput
+          id="frequency"
+          label="Check pose interval"
+          value={config.getPoseFrequency}
+          addDegreeSign={false}
+          onChange={(val) =>
+            dispatchConfig({
+              type: "SET_GET_POSE_FREQUENCY",
+              payload: val,
+            })
+          }
+        />
+
+        <CustomSwitch
+          id="ear-shoulder-monitoring-switch"
+          label="Ear-shoulder angle monitoring"
+          isChecked={config.earShoulderMonitoring.enabled}
+          onChange={() =>
+            dispatchConfig({
+              type: "TOGGLE_EAR_SHOULDER_MONITORING",
+            })
+          }
+        />
+        <CustomInput
+          id="ear-shoulder-angle"
+          label="Desired angle"
+          value={config.earShoulderMonitoring.angle}
+          onChange={(val) =>
+            dispatchConfig({
+              type: "SET_EAR_SHOULDER_ANGLE",
+              payload: val,
+            })
+          }
+        />
+        <CustomInput
+          id="ear-shoulder-tolerance"
+          label="Tolerance"
+          value={config.earShoulderMonitoring.tolerance}
+          onChange={(val) =>
+            dispatchConfig({
+              type: "SET_EAR_SHOULDER_TOLERANCE",
+              payload: val,
+            })
+          }
+        />
+
+        <CustomSwitch
+          id="shoulder-wrist-switch"
+          label="Shoulder-wrist angle monitoring"
+          isChecked={config.shoulderWristMonitoring.enabled}
+          onChange={() =>
+            dispatchConfig({
+              type: "TOGGLE_SHOULDER_WRIST_MONITORING",
+            })
+          }
+        />
+        <CustomInput
+          id="shoulder-wrist-angle"
+          label="Desired angle"
+          value={config.shoulderWristMonitoring.angle}
+          onChange={(val) =>
+            dispatchConfig({
+              type: "SET_SHOULDER_WRIST_ANGLE",
+              payload: val,
+            })
+          }
+        />
+        <CustomInput
+          id="shoulder-wrist-tolerance"
+          label="Tolerance"
+          value={config.shoulderWristMonitoring.tolerance}
+          onChange={(val) =>
+            dispatchConfig({
+              type: "SET_SHOULDER_WRIST_TOLERANCE",
+              payload: val,
+            })
+          }
+        />
+
+        <CustomSwitch
+          id="ban-knee-and-ankle-switch"
+          label="Ban-knee-and-ankle"
+          isChecked={config.banKneeAndAnkle}
+          onChange={() =>
+            dispatchConfig({
+              type: "TOGGLE_BAN_KNEE_AND_ANKLE",
+            })
+          }
+        />
+      </Box>
+    </VStack>
+  );
+};
+
+export default Config;
